b134ddb4cfb66c9b7e7d332f113ad40756e53f57
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-20 18:19:34 +0530

    Update main.tf

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,1 +1,28 @@
-t
+# main.tf (real-time companies style)
+
+module "eks" {
+  source = "../../modules/eks"
+
+  env = var.env
+
+  # Cluster
+  cluster_name     = var.cluster_name
+  cluster_role_name = var.eks_cluster_role_name  # pass name, not ARN
+
+  # Node group
+  node_group_name       = var.node_group_name
+  instance_type         = var.instance_type
+  ami_id                = var.ami_id
+  desired_size          = var.desired_size
+  min_size              = var.min_size
+  max_size              = var.max_size
+  user_data             = var.user_data
+  node_role_name        = var.eks_nodes_ssm_role_name   # pass name
+
+  # Networking
+  subnet_ids         = var.subnet_ids
+  security_group_ids = var.security_group_ids
+
+  # KMS
+  kms_key_arn = var.kms_key_arn
+}
