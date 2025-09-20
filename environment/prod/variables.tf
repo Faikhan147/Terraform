53bcf9581e43e420d5aac212e7bdd69fec2d1e9a
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-20 18:19:04 +0530

    Update variables.tf

--- environment/prod/variables.tf
+++ environment/prod/variables.tf
@@ -1,1 +1,21 @@
-yt
+# variables.tf (real-time companies style)
+
+
+variable "env" {}
+variable "cluster_name" {}
+variable "node_group_name" {}
+variable "instance_type" {}
+variable "desired_size" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "ami_id" {}
+variable "user_data" {}
+variable "kms_key_arn" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "security_group_ids" {
+  type = list(string)
+}
+variable "eks_cluster_role_name" {}
+variable "eks_nodes_ssm_role_name" {}
